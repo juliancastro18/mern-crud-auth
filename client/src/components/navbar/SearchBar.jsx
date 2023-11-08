44aa32a9d5e34f3aff66961d8c82b68c91574b0e
Julián Castro <juliancastro18k@gmail.com>
2023-11-08 01:28:39 -0300

    Minor fix

--- client/src/components/navbar/SearchBar.jsx
+++ client/src/components/navbar/SearchBar.jsx
@@ -9,18 +9,24 @@ function SearchBar({
 }) {
   const { searchNotes, setSearchNotes } = useNotes();
   const autoFocus = isSearchBarOpen && windowSize.width <= 640;
+  const componentRef = useRef(null);
   const searchBarRef = useRef(null);
 
-  const handleClose = () => {
-    setIsSearchBarOpen(false);
+  const handleClickOutside = (e) => {
+    if (!componentRef.current.contains(e.target)) {
+      setIsSearchBarOpen(false);
+    }
   };
 
   return (
-    <div className={className}>
+    <div className={className} ref={componentRef}>
       <div className="relative group">
         <div className="absolute inset-y-0 left-0 flex items-center pl-3">
-          <button className="p-3 hover:bg-zinc-200 hover:bg-opacity-20 rounded-full group-focus-within:hover:bg-opacity-50 cursor-pointer" onClick={() => searchBarRef.current.focus()}>
-            {windowSize.width > 640 ? (
+          {windowSize.width > 640 ? (
+            <button
+              className="p-3 hover:bg-zinc-200 hover:bg-opacity-20 rounded-full group-focus-within:hover:bg-opacity-50 cursor-pointer"
+              onClick={() => searchBarRef.current.focus()}
+            >
               <svg
                 className="w-4 h-4 text-zinc-300 group-focus-within:text-zinc-600 focus:text-zinc-300"
                 aria-hidden="true"
@@ -36,7 +42,12 @@ function SearchBar({
                   d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
                 />
               </svg>
-            ) : (
+            </button>
+          ) : (
+            <button
+              className="p-3 hover:bg-zinc-200 hover:bg-opacity-20 rounded-full group-focus-within:hover:bg-opacity-50 cursor-pointer"
+              onClick={() => setIsSearchBarOpen(false)}
+            >
               <svg
                 className="w-4 h-4 text-zinc-300 group-focus-within:text-zinc-600"
                 xmlns="http://www.w3.org/2000/svg"
@@ -51,8 +62,8 @@ function SearchBar({
                   d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
                 />
               </svg>
-            )}
-          </button>
+            </button>
+          )}
         </div>
         <input
           type="search"
@@ -60,7 +71,7 @@ function SearchBar({
           className="block w-full p-3 px-16 text-base rounded-lg bg-zinc-600 placeholder-zinc-400 focus:bg-white focus:text-zinc-900 outline-none"
           onChange={(e) => setSearchNotes(e.target.value)}
           value={searchNotes}
-          onBlur={handleClose}
+          onBlur={handleClickOutside}
           autoFocus={autoFocus}
           placeholder="Search"
           ref={searchBarRef}
@@ -69,7 +80,10 @@ function SearchBar({
           <div className="absolute inset-y-0 right-0 flex items-center pr-3">
             <button
               className="p-3 hover:bg-zinc-200 hover:bg-opacity-20 rounded-full group-focus-within:hover:bg-opacity-50"
-              onClick={() => setSearchNotes("")}
+              onClick={() => {
+                setSearchNotes("");
+                searchBarRef.current.focus();
+              }}
             >
               <svg
                 className="w-4 h-4 text-zinc-300 group-focus-within:text-zinc-600"
